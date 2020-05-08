6f627e7fa8544587b81967ba9afc7e787cce3f9e
Félix Queiruga <fqueiruga@cloudbees.com>
2020-05-08 12:33:44 +0200

    Extracts version detection to a class

--- src/main/java/io/jenkins/plugins/bootstrap4/api/JenkinsVersion.java
+++ src/main/java/io/jenkins/plugins/bootstrap4/api/JenkinsVersion.java
@@ -4,7 +4,11 @@ import hudson.util.VersionNumber;
 
 public class JenkinsVersion {
 
-    public static boolean isNewerThanOrEqualTo(String current, String target) {
+    public static JenkinsVersion get() {
+        return new JenkinsVersion();
+    }
+
+    public boolean isNewerThanOrEqualTo(String current, String target) {
         return new VersionNumber(current).isNewerThanOrEqualTo(new VersionNumber(target));
     }
 
--- src/test/java/io/jenkins/plugins/bootstrap4/api/JenkinsVersionTest.java
+++ src/test/java/io/jenkins/plugins/bootstrap4/api/JenkinsVersionTest.java
@@ -11,28 +11,28 @@ public class JenkinsVersionTest {
     @Test
     public void isNewerThanOrEqualTo() {
         String current = "2.221.3-SNAPSHOT";
-        assertTrue("Is true for the same version", JenkinsVersion.isNewerThanOrEqualTo(current, "2.221"));
-        assertTrue("Is true for a lower version", JenkinsVersion.isNewerThanOrEqualTo(current, "2.204"));
-        assertFalse("Is false for a higher version", JenkinsVersion.isNewerThanOrEqualTo(current, "2.235"));
+        assertTrue("Is true for the same version", JenkinsVersion.get().isNewerThanOrEqualTo(current, "2.221"));
+        assertTrue("Is true for a lower version", JenkinsVersion.get().isNewerThanOrEqualTo(current, "2.204"));
+        assertFalse("Is false for a higher version", JenkinsVersion.get().isNewerThanOrEqualTo(current, "2.235"));
     }
 
     @Test
     public void detectsVersionsInManyNamingSchemes() {
         String current = "2.235";
-        assertTrue(JenkinsVersion.isNewerThanOrEqualTo(current, "2.235"));
-        assertFalse(JenkinsVersion.isNewerThanOrEqualTo(current, "2.236.1-SNAPSHOT"));
+        assertTrue(JenkinsVersion.get().isNewerThanOrEqualTo(current, "2.235"));
+        assertFalse(JenkinsVersion.get().isNewerThanOrEqualTo(current, "2.236.1-SNAPSHOT"));
 
         current = "2.235.2";
-        assertTrue(JenkinsVersion.isNewerThanOrEqualTo(current, "2.235"));
-        assertFalse(JenkinsVersion.isNewerThanOrEqualTo(current, "2.236.1-SNAPSHOT"));
+        assertTrue(JenkinsVersion.get().isNewerThanOrEqualTo(current, "2.235"));
+        assertFalse(JenkinsVersion.get().isNewerThanOrEqualTo(current, "2.236.1-SNAPSHOT"));
 
         current = "2.235.2.1";
-        assertTrue(JenkinsVersion.isNewerThanOrEqualTo(current, "2.235"));
-        assertFalse(JenkinsVersion.isNewerThanOrEqualTo(current, "2.236.1-SNAPSHOT"));
+        assertTrue(JenkinsVersion.get().isNewerThanOrEqualTo(current, "2.235"));
+        assertFalse(JenkinsVersion.get().isNewerThanOrEqualTo(current, "2.236.1-SNAPSHOT"));
 
         current = "2.235.2-rc123-next-SNAPSHOT";
-        assertTrue(JenkinsVersion.isNewerThanOrEqualTo(current, "2.235"));
-        assertFalse(JenkinsVersion.isNewerThanOrEqualTo(current, "2.236.1-SNAPSHOT"));
+        assertTrue(JenkinsVersion.get().isNewerThanOrEqualTo(current, "2.235"));
+        assertFalse(JenkinsVersion.get().isNewerThanOrEqualTo(current, "2.236.1-SNAPSHOT"));
     }
 
 }
